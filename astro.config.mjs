d4f1d24a9e0cbb21df4dde58dae9577bdb395c9e
Pumia11 <vanalbaobei@vip.qq.com>
2026-03-05 10:31:44 +0800

    optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,17 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'pdfjs': ['pdfjs-dist'],
+            'mammoth': ['mammoth'],
+            'supabase': ['@supabase/supabase-js']
+          }
+        }
+      }
+    }
+  }
 });
