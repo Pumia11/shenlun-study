18c7634ba4ab1f5c83fb499c20e47ec1fd253e2f
Pumia11 <vanalbaobei@vip.qq.com>
2026-03-05 12:01:20 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   vite: {
+    define: {
+      'import.meta.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
+      'import.meta.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY),
+    },
     build: {
       rollupOptions: {
         output: {
